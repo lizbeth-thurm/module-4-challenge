b1d6b2beedb383314055d44b77c8c997dde32795
slthurman01 <slthurman01@gmail.com>
2022-10-11 13:23:58 -0500

    Hides elements at start.

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var startScreen = document.getElementById("start-container");
+var quizScreen = document.getElementById("question-container");
+var finishScreen = document.getElementById("finish-container");
+var scoreScreen = document.getElementById("score-container");
+
+finishScreen.style.display = "none";
+quizScreen.style.display = "none";
+scoreScreen.style.display = "none";
+
+function startFunction() {
+    startScreen.style.display = "none";
+  }
+
