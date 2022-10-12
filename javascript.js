132e72a0f8d3f9f36ec5c0c9555617e3c1c00789
slthurman01 <slthurman01@gmail.com>
2022-10-11 19:41:13 -0500

    Adds check for right or wrong answers and points for them.

--- javascript.js
+++ javascript.js
@@ -4,6 +4,14 @@ var startScreen = document.getElementById("start-container");
 var finishScreen = document.getElementById("finish-container");
 var scoreScreen = document.getElementById("score-container");
 
+// Variables for options boxes
+
+var optionBox1 = document.getElementById("option-container1");
+var optionBox2 = document.getElementById("option-container2");
+var optionBox3 = document.getElementById("option-container3");
+var optionBox4 = document.getElementById("option-container4");
+var optionBox5 = document.getElementById("option-container5");
+
 // These variables represent the containers for each separate set of questions.
 
 var quizScreen1 = document.getElementById("question-container1");
@@ -23,23 +31,108 @@ quizScreen3.style.display = "none";
 quizScreen4.style.display = "none";
 quizScreen5.style.display = "none";
 
-// Variables for controlling correct and incorrect answers in the quiz.
+// Variables for controlling correct and incorrect answers in the quiz and holding score value.
 
 var answer;
 var correctAnswer;
 var wrongAnswer;
+var playerScore;
 
-function correctFunction(){
+// Functions to check if answer was right or wrong, increment score, and display result.
+
+function correctFunction1(){
     answer = true;
+    playerScore = playerScore + 20;
+
+    optionBox1.style.display = "none";
+
+    document.getElementById("right-or-wrong1").innerHTML = "Correct!";
 }
 
-function wrongFunction(){
+function wrongFunction1(){
     answer = false;
+
+    optionBox1.style.display = "none";
+
+    document.getElementById("right-or-wrong1").innerHTML = "Wrong!";
 }
+
+function correctFunction2(){
+    answer = true;
+    playerScore = playerScore + 20;
+
+    optionBox2.style.display = "none";
+
+    document.getElementById("right-or-wrong2").innerHTML = "Correct!";
+}
+
+function wrongFunction2(){
+    answer = false;
+
+    optionBox2.style.display = "none";
+
+    document.getElementById("right-or-wrong2").innerHTML = "Wrong!";
+}
+
+function correctFunction3(){
+    answer = true;
+    playerScore = playerScore + 20;
+
+    optionBox3.style.display = "none";
+
+    document.getElementById("right-or-wrong3").innerHTML = "Correct!";
+}
+
+function wrongFunction3(){
+    answer = false;
+
+    optionBox3.style.display = "none";
+
+    document.getElementById("right-or-wrong3").innerHTML = "Wrong!";
+}
+
+function correctFunction4(){
+    answer = true;
+    playerScore = playerScore + 20;
+
+    optionBox4.style.display = "none";
+
+    document.getElementById("right-or-wrong4").innerHTML = "Correct!";
+}
+
+function wrongFunction4(){
+    answer = false;
+
+    optionBox4.style.display = "none";
+
+    document.getElementById("right-or-wrong4").innerHTML = "Wrong!";
+}
+
+function correctFunction5(){
+    answer = true;
+    playerScore = playerScore + 20;
+
+    optionBox5.style.display = "none";
+
+    document.getElementById("right-or-wrong5").innerHTML = "Correct!";
+}
+
+function wrongFunction5(){
+    answer = false;
+
+    optionBox5.style.display = "none";
+
+    document.getElementById("right-or-wrong5").innerHTML = "Wrong!";
+}
+
+
 
 // Function to start quiz, start timer, and switch to first question screen.
 
 function startFunction() {
+    playerScore = 0;
+
+    document.getElementById("right-or-wrong1").innerHTML = "SELECT ANSWER";
     startScreen.style.display = "none";
     quizScreen1.style.display = "block";
   }
@@ -47,31 +140,45 @@ function startFunction() {
 // Function to advance to next question and log score.
 
 function nextFunction1() {
+
+    document.getElementById("right-or-wrong2").innerHTML = "SELECT ANSWER";
     quizScreen1.style.display = "none";
     quizScreen2.style.display = "block";
 }
 
 function nextFunction2() {
+
+    document.getElementById("right-or-wrong3").innerHTML = "SELECT ANSWER";
     quizScreen2.style.display = "none";
     quizScreen3.style.display = "block";
 }
 
 function nextFunction3() {
+
+    document.getElementById("right-or-wrong4").innerHTML = "SELECT ANSWER";
     quizScreen3.style.display = "none";
     quizScreen4.style.display = "block";
 }
 
 function nextFunction4() {
+
+    document.getElementById("right-or-wrong5").innerHTML = "SELECT ANSWER";
     quizScreen4.style.display = "none";
     quizScreen5.style.display = "block";
 }
 
 function nextFunction5() {
+    document.getElementById("show-score").innerHTML = playerScore;
     quizScreen5.style.display = "none";
     finishScreen.style.display = "block";
 }
 
 function nextFunction6() {
     finishScreen.style.display = "none";
-    scoreScreen.style.displat = "block";
+    scoreScreen.style.display = "block";
+}
+
+function nextFunction7() {
+    scoreScreen.style.display = "none";
+    startScreen.style.display = "block";
 }
